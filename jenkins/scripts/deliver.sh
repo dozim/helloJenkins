b93d48d48d2a20b6d9e7c383843432ce3d21715e
dzimmermann <dzimmermann@optica.de>
2019-09-02 14:40:08 +0200

    Uncomment run of jar at the end of the pipeline.

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,5 +23,5 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-#java -jar target/${NAME}-${VERSION}.jar
+java -jar target/${NAME}-${VERSION}.jar
 unzip -q -c target/${NAME}-${VERSION}.jar META-INF/MANIFEST.MF
